d5b7b186507727ae9e036a0d4c9191397e30e5a5
bloup <preteseille06@gmail.com>
2025-11-05 23:33:47 +0100

    Configure Java 21 release and enable multi-release JAR; update Paper/Spigot to 1.20.x; improve BlurpNettyHelper to resolve CraftPlayer dynamically and fallback on networkManager field.

--- src/main/java/fr/bloup/blurpapi/utils/BlurpNettyHelper.java
+++ src/main/java/fr/bloup/blurpapi/utils/BlurpNettyHelper.java
@@ -1,25 +1,50 @@
 package fr.bloup.blurpapi.utils;
 
 import io.netty.channel.Channel;
-import org.bukkit.craftbukkit.v1_21_R2.entity.CraftPlayer;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class BlurpNettyHelper {
     public static Channel getChannel(Player player) {
         try {
-            Object handle = ((CraftPlayer) player).getHandle();
-            Field connectionField = handle.getClass().getField("connection");
+            String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+
+            Class<?> craftPlayerClass = Class.forName("org.bukkit.craftbukkit." + version + ".entity.CraftPlayer");
+
+            Method getHandle = craftPlayerClass.getMethod("getHandle");
+            Object handle = getHandle.invoke(player);
+
+            Field connectionField = handle.getClass().getDeclaredField("connection");
+            connectionField.setAccessible(true);
             Object connection = connectionField.get(handle);
 
-            Field networkManagerField = connection.getClass().getDeclaredField("connection"); // ou "networkManager"
+            Field networkManagerField = null;
+            try {
+                networkManagerField = connection.getClass().getDeclaredField("connection");
+            } catch (NoSuchFieldException e1) {
+                try {
+                    networkManagerField = connection.getClass().getDeclaredField("networkManager");
+                } catch (NoSuchFieldException e2) {
+                    try {
+                        networkManagerField = connection.getClass().getDeclaredField("b");
+                    } catch (NoSuchFieldException e3) {
+                        throw new RuntimeException("Impossible de trouver le NetworkManager field");
+                    }
+                }
+            }
+
             networkManagerField.setAccessible(true);
             Object networkManager = networkManagerField.get(connection);
 
+            // Récupère le Channel
             Field channelField = networkManager.getClass().getDeclaredField("channel");
             channelField.setAccessible(true);
+
             return (Channel) channelField.get(networkManager);
+
         } catch (Exception e) {
             e.printStackTrace();
             return null;
