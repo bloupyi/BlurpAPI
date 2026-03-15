81ada31b06837bcf2126171c03f02134de963f5d
bloup <noa.pret@gmail.com>
2026-03-15 23:47:37 +0100

    Add world check in BlurpRegionListener to prevent region transition across worlds

--- src/main/java/fr/bloup/blurpapi/regions/BlurpRegionListener.java
+++ src/main/java/fr/bloup/blurpapi/regions/BlurpRegionListener.java
@@ -23,6 +23,9 @@ public class BlurpRegionListener implements Listener {
         Location to = event.getTo();
         if (from == null || to == null) return;
 
+        // Check if the worlds are different
+        if (!from.getWorld().equals(to.getWorld())) return;
+
         List<String> wasIn = blurpRegion.regionsAt(from);
         List<String> isIn = blurpRegion.regionsAt(to);
 
